c4cf2cdecd15ff82c1a3b4f62ea6564af9f7a640
Lukas Schreiner <lukas.schreiner@fls-wiesbaden.de>
2018-08-19 20:46:37 +0200

    Added imageformats to the directory; path fixed defined for windows, added zip file creation

--- setup.py
+++ setup.py
@@ -9,8 +9,19 @@ import os.path
 import subprocess
 import shlex
 import glob
+import zipfile
+import shutil
 from cx_Freeze import setup, Executable
 scriptDir = os.path.dirname(os.path.realpath(__file__))
+buildDir = os.path.join(scriptDir, 'build')
+distDir = os.path.join(scriptDir, 'dist')
+
+def addToZip(zipFile, p):
+	if os.path.isdir(p):
+		for fls in glob.glob(os.path.join(p, '*'), recursive=True):
+			addToZip(zipFile, fls)
+	else:
+		zipFile.write(p, os.path.relpath(p, buildDir))
 
 files = [
 	os.path.join(scriptDir, 'config.ini.sample'),
@@ -20,12 +31,20 @@ if os.path.exists(os.path.join(scriptDir, 'config.ini')):
 
 # really dirty hack for Windows Server
 if sys.platform == 'win32':
-	platformsPath = os.path.join(
-	os.path.dirname(sys.executable),
+	# Platforms plugins
+	pluginPath = os.path.join(
+		os.path.dirname(sys.executable),
 		'..\\Lib\\site-packages\\PyQt5\\Qt\\plugins\\platforms'
 	)
-	for f in glob.glob(platformsPath + '\\*.dll'):
+	for f in glob.glob(pluginPath + '\\*.dll'):
 		files.append((f, 'platforms\\' + os.path.basename(f)))
+	# Image formats plugins
+	pluginPath = os.path.join(
+		os.path.dirname(sys.executable),
+		'..\\Lib\\site-packages\\PyQt5\\Qt\\plugins\\imageformats'
+	)
+	for f in glob.glob(pluginPath + '\\*.dll'):
+		files.append((f, 'imageformats\\' + os.path.basename(f)))
 
 # DEFAULT VALUES
 setupVersion = "4.27"
@@ -52,7 +71,7 @@ elif variant == 'sds':
 	setupName = 'SDS Vertretungsplaner'
 	setupGuid = '7CE997E4-8D0A-4406-A3EA-F48CAB29F4A9'
 setupIco = os.path.join(scriptDir, 'pixmaps', setupSrcIco)
-#files.append((setupIco, 'logo.ico'))
+files.append((setupIco, 'logo.ico'))
 
 base = None
 exeName = 'flsvplan'
@@ -82,7 +101,7 @@ buildOpts = {
 	'include_files': files,
 	'zip_include_packages': ['PyQt5.QtNetwork', 'PyQt5.sip', 'PyQt5.QtCore', 'PyQt5.QtGui', 'PyQt5.QtWidgets', 'PyQt5', 'sip'],
 	'include_msvcr': True,
-	'build_exe': os.path.join('build', 'vplan-{:s}'.format(setupVersion))
+	'build_exe': os.path.join(buildDir, 'vplan-{:s}'.format(setupVersion))
 }
 
 setup(
@@ -112,9 +131,28 @@ if sys.platform == "win32":
 		publisher=setupPublisher,
 		url=setupUrl,
 		exeName=exeName,
-		buildDirectory=os.path.join('build', 'vplan-{:s}'.format(setupVersion)),
+		buildDirectory=os.path.join(buildDir, 'vplan-{:s}'.format(setupVersion)),
 		appGuid=setupGuid
 	)
 	exeCmd = shlex.split(cmd)
 	p = subprocess.Popen(exeCmd)
 	p.communicate()
+	# copy setup file to build directory.
+	srcSetupFile = os.path.join(scriptDir, 'Output', setupName + '_' + setupVersion + '_setup.exe')
+	dstSetupFile = os.path.join(buildDir, setupName + '_' + setupVersion + '_setup.exe')
+	if os.path.exists(srcSetupFile):
+		shutil.copyfile(srcSetupFile, dstSetupFile)
+
+# create dist file.
+if not os.path.exists(distDir):
+	try:
+		os.makedirs(distDir)
+	except:
+		pass
+
+distZipName = os.path.join(distDir, 'vplan-{:s}.zip'.format(setupVersion))
+zf = zipfile.ZipFile(distZipName, 'w', compression=zipfile.ZIP_DEFLATED)
+for fls in glob.glob(os.path.join(buildDir, 'vplan-{:s}'.format(setupVersion), '*'), recursive=True):
+	addToZip(zf, fls)
+
+zf.close()
